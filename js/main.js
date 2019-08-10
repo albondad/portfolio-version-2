a1fa3417be4a6c1952fdbd5c05667395a3f27024
albondad <36680782+albondad@users.noreply.github.com>
2019-08-10 16:27:45 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,29 +7,25 @@ class Project {
         this.projectSource = projectSource;
         this.viewOnType = viewOnType;
         this.viewOnSource = viewOnSource;
-
     }
     getHTML() {
         var tagsToClassNames = "";
         for (i=0; i<this.tags.length; i++) {
-            console.log(tagsToClassNames);
-            tagsToClassNames += "tag_" + this.tags[i].replace(/\s+/g, "-").toLowerCase();
+            tagsToClassNames += " tag_" + this.tags[i].replace(/\s+/g, "-").toLowerCase();
         }
-        return '<div class="projectRow col-4 p-2 ' + tagsToClassNames + '">' +
+        return '<div class="projectRow col-xs-12 col-sm-12 col-md-6 col-lg-4 p-2' + tagsToClassNames + '">' +
             '<div class="projectContainer container-fluid">' +
                 '<div class="row">' +
                     '<!--project thumbnail-->' +
-                    '<div class="col-12 p-0">' +
-                        '<a href="' + this.projectSource + '" target="_blank"><img src="' + this.thumbnailSource + '".jpg" class="projectThumbnail"></img></a>' +
-                    '</div>' +
+                    '<a href="' + this.projectSource + '" target="_blank">' +
+                        '<div class="col-12 p-0 projectThumbnailContainer">' +
+                            '<img src="' + this.thumbnailSource + '".jpg" class="projectThumbnail"></img>' +
+                        '</div>' +
+                    '</a>' +
                     '<div class="projectDetails col-12 pt-2 pb-2">' +
-                        '<p class="m-0">' +
-                            '<a href="' + this.projectSource + '" target="_blank"><span class="projectTitle">' + this.title + '</span></a>' +
-                            '<br/>' +
-                            '<span class="projectCategory">' + this.tags[0] + '</span>' +
-                            '<br/>' +
-                            '<a href="' + this.viewOnSource + '" target="_blank"><span class="projectViewOn">View on ' + this.viewOnType + '</span></a>' +
-                        '</p>' +
+                        '<div><a href="' + this.projectSource + '" target="_blank" class="link"><span class="projectTitle">' + this.title + '</span></a></div>' +
+                        '<div class="pt-1"><span class="projectCategory">' + this.tags[0] + '</span></div>' +
+                        '<div><a href="' + this.viewOnSource + '" target="_blank" class="link"><span class="projectViewOn">View on ' + this.viewOnType + '</span></a></div>' +
                     '</div>' +
                 '</div>' +
             '</div>' +
@@ -39,43 +35,96 @@ class Project {
 
 //VARIABLES
 var projects = [];
-var tags = ["Web Development", "Application", "Script", "Highschool", "Assignment", "Personal Project",];
+var tags = ["Web", "App", "Script", "Game", "Montage", "Animation", "Timelapse"];
 var selectedTags = []
+var projectsShown = 0;
+var showViewOns = false;
 
 
 //FUNCTIONS
 function getTagHTML(tagName) {
-    return '<span class="filterTag">' + tagName + '</span>';
+    return '<span class="filterTag">' + tagName + '</span> ';
 }
 function getTagClassName(tagName) {
     return ".tag_" + tagName.replace(/\s+/g, "-").toLowerCase()
 }
 function displayTags() {
     for (i=0; i<tags.length; i++) {
-        $("#filterTags").append(getTagHTML(tags[i]))
+        $("#filterTags").append(getTagHTML(tags[i]));
     }
 }
 function displayProjects() {
-    for (i=0; i<projects.length; i++) {
-        $("#projects").append(projects[i].getHTML());
+    for (i=0; i<projectsData.length; i++) {
+        projects.push(new Project(projectsData[i].title, projectsData[i].tags, projectsData[i].thumbnailSource, projectsData[i].projectSource, projectsData[i].viewOnType, projectsData[i].viewOnSource));
+    }
+    projects.forEach(function(project) {
+        projectsShown += 1;
+        $("#projects").append(project.getHTML());
+        projectsShown = projects.length;
+    });
+}
+function refreshProjects() {
+    //hides all projects
+    $("#section_projects").fadeTo(500, 0).hide(0, function() {
+        $(".projectRow").hide(0)
+
+        for (i=0; i<selectedTags.length; i++) {
+            $(getTagClassName(selectedTags[i])).show(0)
+        }
+        if (selectedTags.length == 0) {
+            $("#tag_all").addClass("filterTagSelected")
+            $(".projectRow").show(0);
+        }
+        $("#section_projects").show(0).fadeTo(500, 1);;
+    });
+}
+function updateShowViewOns() {
+    if (window.innerWidth > 991) {
+        showViewOns = false;
+        $(".projectContainer").find(".projectViewOn").parent().parent().hide(0);
+    }
+    else {
+        showViewOns = true;
+        $(".projectContainer").find(".projectViewOn").parent().parent().show(0);
     }
 }
 
+
 //ON DOCUMENT LOAD
 $(function() {
+    displayProjects();
+    displayTags();
+    updateShowViewOns();
 
-    $.getJSON("test.json", function(file) {
-        var newProjects=file.projects;
-        for (i=0; i<newProjects.length; i++) {
-            projects.push(new Project(newProjects[i].title, newProjects[i].tags, newProjects[i].thumbnailSource, newProjects[i].projectSource, newProjects[i].viewOnType, newProjects[i].viewOnSource));
+    //dealing with navigation links
+    $("#navigation_contacts").click(function() {
+        if (!$("#section_projects").is(":hidden")) {
+            $("#section_filters").fadeTo(500, 0).hide(0);
+            $("#section_projects").fadeTo(500, 0).hide(0, function() {
+                $("#section_contacts").show().fadeTo(500, 1);
+            });
         }
-        displayProjects();
+    });
+    $("#navigation_projects").click(function() {
+        if (!$("#section_contacts").is(":hidden")) {
+            $("#section_contacts").fadeTo(500, 0).hide(0, function() {
+                $("#section_projects").show(0).fadeTo(500, 1);
+                $("#section_filters").show(0).fadeTo(500, 1);
+            });
+        }
     });
 
-    displayTags();
-
+    //deals when tags are clicked
     $(".filterTag").click(function() {
-        target = $(this)
+        var target = $(this)
+
+        //checks if "all" tag clicked
+        if (target.attr("id") == "tag_all") {
+            $(".filterTagSelected").removeClass("filterTagSelected");
+            selectedTags = [];
+        }
+
+        //updates tag visuals when clicked
         if (target.hasClass("filterTagSelected")) {
             target.removeClass("filterTagSelected");
             selectedTags = selectedTags.filter(function(tag) {
@@ -84,12 +133,59 @@ $(function() {
         }
         else {
             target.addClass("filterTagSelected");
-            selectedTags.push(target.html());
+            $("#tag_all").removeClass("filterTagSelected")
+            if (target.html() != "All") {
+                selectedTags.push(target.html());
+            }
         }
 
-        for (i=0; i<selectedTags.length; i++) {
-            $(getTagClassName(selectedTags[i])).css("display", "block");
-        }
-        console.log(selectedTags);
+        refreshProjects();
     });
+
+    //animation for showing and hiding "view on" information
+    $(".projectContainer").mouseenter(function() {
+        if (!showViewOns) {
+            $(this).find(".projectViewOn").parent().parent().slideDown(500);
+        }
+    });
+    $(".projectContainer").mouseleave(function() {
+        if (!showViewOns) {
+            $(this).find(".projectViewOn").parent().parent().slideUp(500);
+        }
+    });
+
+    //dealing with loading screen
+    var loadingAnimation = setInterval(function() {
+        if($("#txt_loading").text() == "loading") {
+            $("#txt_loading").html("loading.")
+        }
+        else if($("#txt_loading").text() == "loading.") {
+            $("#txt_loading").html("loading..")
+        }
+        else if($("#txt_loading").text() == "loading..") {
+            $("#txt_loading").html("loading...")
+        }
+        else if($("#txt_loading").text() == "loading...") {
+            $("#txt_loading").html("loading")
+        }
+    }, 500);
+    var checkIfIconsLoaded = setInterval(function() {
+        if ($("#lastIcon").html() != "") {
+            clearInterval(checkIfIconsLoaded);
+            setTimeout (function() {
+                $("#section_contacts").hide(0);
+                $("#section_loadingScreen").fadeTo(500, 0).hide(function() {
+                    $("#section_navigation").fadeTo(500, 1);
+                    $("#section_filters").fadeTo(500, 1);
+                    $("#section_projects").fadeTo(500, 1);
+                });
+            }, 1000);
+        }
+    }, 1000);
+});
+
+
+//ON WINDOW RESIZED LOAD
+$(window).resize(function() {
+    updateShowViewOns();
 });
---
+++ js/projectsData.js
@@ -0,0 +1,226 @@
+var projectsData = [
+    {
+        "title": "YouTube Channel Stats Tracker",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-youTubeChannelStatsTracker.jpg",
+        "projectSource": "https://albondad-youTubeChannelStatsTracker.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_youTubeChannelStatsTracker"
+    },
+    {
+        "title": "HydraForce DWG File Organizer",
+        "tags": ["Web", "Script"],
+        "thumbnailSource": "imgs/thumbnail-hydraForceDWGFileOrganizer.jpg",
+        "projectSource": "https://github.com/albondad/script_hydraForceDWGFileOrganizer",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://github.com/albondad/script_hydraForceDWGFileOrganizer"
+    },
+    {
+        "title": "Blast 2019",
+        "tags": ["Montage"],
+        "thumbnailSource": "imgs/thumbnail-blast2019.jpg",
+        "projectSource": "https://youtu.be/e8r2msCBbCQ",
+        "viewOnType": "YouTube",
+        "viewOnSource": "https://youtu.be/e8r2msCBbCQ"
+    },
+    {
+        "title": "Bad Site Remake",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-badSiteRemake.jpg",
+        "projectSource": "https://albondad-badSiteRemake.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_badSiteRemake"
+    },
+    {
+        "title": "Shark Bank",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-sharkBank.jpg",
+        "projectSource": "https://albondad-sharkBank.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_sharkBank"
+    },
+    {
+        "title": "WordPress Portfolio",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-wordPressPortfolio.jpg",
+        "projectSource": "https://bondadportfolio.wordpress.com/",
+        "viewOnType": "Wordpress",
+        "viewOnSource": "https://crossingfitness.wordpress.com/"
+    },
+    {
+        "title": "WordPress Client Site",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-wordPressClientSite.jpg",
+        "projectSource": "https://crossingfitness.wordpress.com/",
+        "viewOnType": "Wordpress",
+        "viewOnSource": "https://crossingfitness.wordpress.com/"
+    },
+    {
+        "title": "Guess Who",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-guessWho.jpg",
+        "projectSource": "https://albondad-guessWho.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_guessWho"
+    },
+    {
+        "title": "Pizza Site",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-pizzaSite.jpg",
+        "projectSource": "https://albondad-pizzaSite.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_pizzaSite"
+    },
+    {
+        "title": "Band Page",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-bandPage.jpg",
+        "projectSource": "https://albondad-bandPage.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_bandPage"
+    },
+    {
+        "title": "Photography Website",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-photographyWebsite.jpg",
+        "projectSource": "https://albondad-photographyWebsite.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_photographyWebsite"
+    },
+    {
+        "title": "CSS Grid Practice",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-cssGridPractice.jpg",
+        "projectSource": "https://albondad-cssGridPractice.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_cssGridPractice"
+    },
+    {
+        "title": "Parallax Site",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-parallaxSite.jpg",
+        "projectSource": "https://albondad-parallaxSite.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_parallaxSite"
+    },
+    {
+        "title": "960 Grid Practice",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-960GridPractice.jpg",
+        "projectSource": "https://albondad-960GridPractice.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_960GridPractice"
+    },
+    {
+        "title": "Half Face",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-halfFace.jpg",
+        "projectSource": "https://albondad-halfFace.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_halfFace"
+    },
+    {
+        "title": "Two Movies Poster",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-twoMoviesPoster.jpg",
+        "projectSource": "https://albondad-twoMoviesPoster.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_twoMoviePosters"
+    },
+    {
+        "title": "Spot The Difference",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-spotTheDifference.jpg",
+        "projectSource": "https://albondad-spotTheDifference.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_spotTheDifference"
+    },
+    {
+        "title": "Travel Site",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-travelSite.jpg",
+        "projectSource": "https://albondad-travelSite.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_travelSite"
+    },
+    {
+        "title": "John Lee Biography",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-johnLeeBiography.jpg",
+        "projectSource": "https://johnLeeBiography.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_JohnLeeBiography"
+    },
+    {
+        "title": "Button Version 2",
+        "tags": ["Web", "Personal Project"],
+        "thumbnailSource": "imgs/thumbnail-buttonv2.jpg",
+        "projectSource": "https://albondad-button-v2.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_button_v2"
+    },
+    {
+        "title": "Button Version 1",
+        "tags": ["Web", "Personal Project"],
+        "thumbnailSource": "imgs/thumbnail-buttonv1.jpg",
+        "projectSource": "https://albondad-button-v2.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_button_v1"
+    },
+    {
+        "title": "Four Favorites",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-fourFavorites.jpg",
+        "projectSource": "https://albondad-fourfavorites.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_fourFavorites"
+    },
+    {
+        "title": "Screen Dimensions",
+        "tags": ["Web"],
+        "thumbnailSource": "imgs/thumbnail-screenDimensions.jpg",
+        "projectSource": "https://albondad-screenDimensions.netlify.com",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/site_screenDimensions"
+    },
+    {
+        "title": "Boxxy Run",
+        "tags": ["Game"],
+        "thumbnailSource": "imgs/thumbnail-boxxyRun.jpg",
+        "projectSource": "https://GitHub.com/albondad/game_boxxyRun",
+        "viewOnType": "GitHub",
+        "viewOnSource": "https://GitHub.com/albondad/game_boxxyRun"
+    },
+    {
+        "title": "Low Poly Car",
+        "tags": ["Timelapse"],
+        "thumbnailSource": "imgs/thumbnail-lowPolyCar.jpg",
+        "projectSource": "https://www.youtube.com/watch?v=TMrQG1m0Rmc",
+        "viewOnType": "YouTube",
+        "viewOnSource": "https://www.youtube.com/watch?v=TMrQG1m0Rmc"
+    },
+    {
+        "title": "Low Poly Christmas Scene",
+        "tags": ["Timelapse"],
+        "thumbnailSource": "imgs/thumbnail-lowPolySimpleCharacter.jpg",
+        "projectSource": "https://www.youtube.com/watch?v=AHysxTjh-X4",
+        "viewOnType": "YouTube",
+        "viewOnSource": "https://www.youtube.com/watch?v=AHysxTjh-X4"
+    },
+    {
+        "title": "Low Poly Christmas Scene",
+        "tags": ["Timelapse"],
+        "thumbnailSource": "imgs/thumbnail-lowPolyChristmasScene.jpg",
+        "projectSource": "https://www.youtube.com/watch?v=P7HtjaFSq2A",
+        "viewOnType": "YouTube",
+        "viewOnSource": "https://www.youtube.com/watch?v=P7HtjaFSq2A"
+    },
+    {
+        "title": "Stick Man Character",
+        "tags": ["Timelapse"],
+        "thumbnailSource": "imgs/thumbnail-stickManCharacter.jpg",
+        "projectSource": "https://www.youtube.com/watch?v=7itGpO4DgbA",
+        "viewOnType": "YouTube",
+        "viewOnSource": "https://www.youtube.com/watch?v=7itGpO4DgbA"
+    }
+]
