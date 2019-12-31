ae76d567811657d97fcdfedc597a99ccca462504
albondad <36680782+albondad@users.noreply.github.com>
2019-12-30 20:51:07 -0600

    Update projectsData.js

--- js/projectsData.js
+++ js/projectsData.js
@@ -10,15 +10,6 @@ var projectsData = [
     },
     {
         "show": true,
-        "title": "Highschool Assignments",
-        "tags": ["Development"],
-        "thumbnailSource": "imgs/thumbnail-highschoolAssignments.jpg",
-        "projectSource": "https://github.com/albondad/site_portfolio_v1?files=1",
-        "viewOnType": "GitHub",
-        "viewOnSource": "https://github.com/albondad/site_portfolio_v1?files=1"
-    },
-    {
-        "show": true,
         "title": "Blast 2019",
         "tags": ["Videography"],
         "thumbnailSource": "imgs/thumbnail-blast2019.jpg",
@@ -28,21 +19,12 @@ var projectsData = [
     },
     {
         "show": true,
-        "title": "Button Clicker Version",
+        "title": "Button Clicker",
         "tags": ["Development", "Personal Project"],
         "thumbnailSource": "imgs/thumbnail-buttonv2.jpg",
         "projectSource": "https://albondad-button-v2.netlify.com",
         "viewOnType": "GitHub",
         "viewOnSource": "https://GitHub.com/albondad/site_button_v2?files=1"
-    },
-    {
-        "show": true,
-        "title": "Boxxy Run",
-        "tags": ["Development"],
-        "thumbnailSource": "imgs/thumbnail-boxxyRun.jpg",
-        "projectSource": "https://GitHub.com/albondad/Development_boxxyRun?files=1",
-        "viewOnType": "GitHub",
-        "viewOnSource": "https://GitHub.com/albondad/Development_boxxyRun?files=1"
     },
     {
         "show": true,
@@ -61,24 +43,6 @@ var projectsData = [
         "projectSource": "https://www.youtube.com/watch?v=qZqZk6mMLwM",
         "viewOnType": "YouTube",
         "viewOnSource": "https://www.youtube.com/watch?v=qZqZk6mMLwM"
-    },
-    {
-        "show": true,
-        "title": "Prison Packs 2017",
-        "tags": ["Videography"],
-        "thumbnailSource": "imgs/thumbnail-prisonPacks2017.jpg",
-        "projectSource": "https://www.youtube.com/watch?v=pREOF4VgHGI",
-        "viewOnType": "YouTube",
-        "viewOnSource": "https://www.youtube.com/watch?v=pREOF4VgHGI"
-    },
-    {
-        "show": true,
-        "title": "World's Fair, October 2017",
-        "tags": ["Videography"],
-        "thumbnailSource": "imgs/thumbnail-worldsFairOctober2017.jpg",
-        "projectSource": "https://www.youtube.com/watch?v=E4y9j-xr4ao",
-        "viewOnType": "YouTube",
-        "viewOnSource": "https://www.youtube.com/watch?v=E4y9j-xr4ao"
     },
     {
         "show": true,
@@ -107,40 +71,4 @@ var projectsData = [
         "viewOnType": "YouTube",
         "viewOnSource": "https://www.youtube.com/watch?v=9EbHwQtMgMo"
     },
-    {
-        "show": true,
-        "title": "Low Poly Car",
-        "tags": ["Videography"],
-        "thumbnailSource": "imgs/thumbnail-lowPolyCar.jpg",
-        "projectSource": "https://www.youtube.com/watch?v=TMrQG1m0Rmc",
-        "viewOnType": "YouTube",
-        "viewOnSource": "https://www.youtube.com/watch?v=TMrQG1m0Rmc"
-    },
-    {
-        "show": true,
-        "title": "Low Poly Christmas Scene",
-        "tags": ["Videography"],
-        "thumbnailSource": "imgs/thumbnail-lowPolySimpleCharacter.jpg",
-        "projectSource": "https://www.youtube.com/watch?v=AHysxTjh-X4",
-        "viewOnType": "YouTube",
-        "viewOnSource": "https://www.youtube.com/watch?v=AHysxTjh-X4"
-    },
-    {
-        "show": true,
-        "title": "Low Poly Christmas Scene",
-        "tags": ["Videography"],
-        "thumbnailSource": "imgs/thumbnail-lowPolyChristmasScene.jpg",
-        "projectSource": "https://www.youtube.com/watch?v=P7HtjaFSq2A",
-        "viewOnType": "YouTube",
-        "viewOnSource": "https://www.youtube.com/watch?v=P7HtjaFSq2A"
-    },
-    {
-        "show": true,
-        "title": "Stick Man Character",
-        "tags": ["Videography"],
-        "thumbnailSource": "imgs/thumbnail-stickManCharacter.jpg",
-        "projectSource": "https://www.youtube.com/watch?v=7itGpO4DgbA",
-        "viewOnType": "YouTube",
-        "viewOnSource": "https://www.youtube.com/watch?v=7itGpO4DgbA"
-    }
 ]
