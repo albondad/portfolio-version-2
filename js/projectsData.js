4ae15efc879554aa8bb98058a3c051432630f60b
albondad <36680782+albondad@users.noreply.github.com>
2020-01-02 00:29:57 -0600

    Update projectsData.js

--- js/projectsData.js
+++ js/projectsData.js
@@ -1,12 +1,21 @@
 var projectsData = [
     {
         "show": true,
-        "title": "Blok Scheduler",
+        "title": "Block Scheduler",
         "tags": ["Development"],
         "thumbnailSource": "imgs/thumbnail-blokScheduler.jpg",
         "projectSource": "https://albondad-blokScheduler.netlify.com",
         "viewOnType": "GitHub",
         "viewOnSource": "https://GitHub.com/albondad/site_blok-scheduler?files=1"
+    },
+    {
+        "show": true,
+        "title": "Block Scheduler Design",
+        "tags": ["Design"],
+        "thumbnailSource": "imgs/thumbnail-blokScheduler.jpg",
+        "projectSource": "https://www.upwork.com/o/profiles/users/_~014416684418a82ffe/?p=1212617702696902656",
+        "viewOnType": "Upwork",
+        "viewOnSource": "https://www.upwork.com/o/profiles/users/_~014416684418a82ffe/?p=1212617702696902656"
     },
     {
         "show": true,
@@ -16,6 +25,15 @@ var projectsData = [
         "projectSource": "https://albondad-youTubeChannelStatsTracker.netlify.com",
         "viewOnType": "GitHub",
         "viewOnSource": "https://GitHub.com/albondad/site_youTubeChannelStatsTracker?files=1"
+    },
+    {
+        "show": true,
+        "title": "YouTube Channel Stats Tracker Design",
+        "tags": ["Development"],
+        "thumbnailSource": "imgs/thumbnail-youTubeChannelStatsTracker.jpg",
+        "projectSource": "https://www.upwork.com/o/profiles/users/_~014416684418a82ffe/?p=1212618701374849024",
+        "viewOnType": "Upwork",
+        "viewOnSource": "https://www.upwork.com/o/profiles/users/_~014416684418a82ffe/?p=1212618701374849024"
     },
     {
         "show": true,
